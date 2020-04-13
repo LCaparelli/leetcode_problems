c2b2342c1bd2ca89b7b52dfdfeb293429505d3e1
Lucas Caparelli <lucas.caparelli112@gmail.com>
2020-04-13 12:26:50 -0300

    Maximum Subarray challenge

--- 30-day-challenge/maximum_subarray.go
+++ 30-day-challenge/maximum_subarray.go
@@ -1,1 +1,42 @@
+/*
+ * Maximum Subarray
+ *
+ * Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum
+ * and return its sum.
+ *
+ * Example:
+ *
+ * Input: [-2,1,-3,4,-1,2,1,-5,4],
+ * Output: 6
+ * Explanation: [4,-1,2,1] has the largest sum = 6.
+ *
+ * Follow up:[]int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
+ *
+ * If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which
+ * is more subtle.
+ */
+
+// NOTE: not sure if they meant to imply the divide and conquer approach is faster, but it's O(n log n). O(n) is the
+// optimal solution for this problem.
+
 package challenge
+
+import "math"
+
+func maxSubArray(nums []int) int {
+	bestSum := math.MinInt32
+	currSum := 0
+
+	for _, number := range nums {
+		currSum = max(number, currSum + number)
+		bestSum = max(currSum, bestSum)
+	}
+	return bestSum
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
--- 30-day-challenge/maximum_subarray_test.go
+++ 30-day-challenge/maximum_subarray_test.go
@@ -1,1 +1,25 @@
 package challenge
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMaxSubArray(t *testing.T) {
+	var tests = []struct {
+		numbers []int
+		want    int
+	}{
+		{[]int{-2, 1, -3, 4, -1, 2, 1, -5, 4}, 6},
+	}
+
+	for _, test := range tests {
+		testName := fmt.Sprintf("Input: %v", test.numbers)
+		t.Run(testName, func(t *testing.T) {
+			ans := maxSubArray(test.numbers)
+			if ans != test.want {
+				t.Errorf("got %v, want %v", ans, test.want)
+			}
+		})
+	}
+}
