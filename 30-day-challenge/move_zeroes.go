4d6545b36a7032d90722c0581f425ae3278368dd
Lucas Caparelli <lucas.caparelli112@gmail.com>
2020-04-13 14:42:56 -0300

    Move Zeroes challenge

---
+++ 30-day-challenge/move_zeroes.go
@@ -0,0 +1,48 @@
+/*
+ * Move Zeroes
+ * 
+ * Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the
+ * non-zero elements.
+ * 
+ * Example:
+ * 
+ * Input: [0,1,0,3,12]
+ * Output: [1,3,12,0,0]
+ * 
+ * Note:
+ * 
+ * You must do this in-place without making a copy of the array.
+ * Minimize the total number of operations.
+ */
+
+package challenge
+
+func moveZeroes(nums []int) {
+	var i int
+	allZeroes := true
+
+	// deals with arrays already ending with one or more zeroes
+	for j := len(nums) - 1; j >= 0; j-- {
+		if nums[j] != 0 {
+			i = j
+			allZeroes = false
+			break
+		}
+	}
+	if allZeroes {
+		return
+	}
+
+	for ; i >= 0; i-- {
+		if nums[i] == 0 {
+			removeAndReplace(i, nums)
+		}
+	}
+}
+
+func removeAndReplace(i int, nums []int) {
+	for ; i < len(nums) - 1; i++ {
+		nums[i] = nums[i + 1]
+	}
+	nums[len(nums) - 1] = 0
+}
---
+++ 30-day-challenge/move_zeroes_test.go
@@ -0,0 +1,27 @@
+package challenge
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMoveZeroes(t *testing.T) {
+	var tests = []struct {
+		numbers []int
+		want    []int
+	}{
+		{[]int{0,1,0,3,12}, []int{1,3,12,0,0}},
+		{[]int{0,1,0}, []int{1,0,0}},
+		{[]int{0,0}, []int{0,0}},
+	}
+
+	for _, test := range tests {
+		testName := fmt.Sprintf("Input: %v", test.numbers)
+		t.Run(testName, func(t *testing.T) {
+			moveZeroes(test.numbers)
+			if !areEqual(test.numbers, test.want) {
+				t.Errorf("got %v, want %v", test.numbers, test.want)
+			}
+		})
+	}
+}
