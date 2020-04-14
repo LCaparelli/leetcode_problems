eca214421119bc83fbae77c9bae1e8fd9b0270fc
Lucas Caparelli <lucas.caparelli112@gmail.com>
2020-04-14 11:43:49 -0300

    Counting Elements challenge

---
+++ 30-day-challenge/week-1/counting_elements_test.go
@@ -0,0 +1,28 @@
+package challenge
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestCountElements(t *testing.T) {
+	var tests = []struct {
+		numbers	[]int
+		want    int
+	}{
+		{[]int{1,2,3}, 2},
+		{[]int{1,1,3,3,5,5,7,7}, 0},
+		{[]int{1,3,2,3,5,0}, 3},
+		{[]int{1,1,2,2}, 2},
+	}
+
+	for _, test := range tests {
+		testName := fmt.Sprintf("Input: %v", test.numbers)
+		t.Run(testName, func(t *testing.T) {
+			ans := countElements(test.numbers)
+			if test.want != ans {
+				t.Errorf("got %v, want %v", ans, test.want)
+			}
+		})
+	}
+}
